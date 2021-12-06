9b5920e0cf6e3a0cad86f2a71025b9a45dd64866
edmondsket <dannew123@gmail.com>
2021-12-06 18:43:59 +0800

    Update site

--- conf.py
+++ conf.py
@@ -9,9 +9,10 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 template = {
-    "name": "Galileo2",
-    "type": "local",
-    "path": "../Templates/Galileo2"
+    "name": "Galileo",
+    "type": "git",
+    "url": "https://github.com/AlanDecode/Maverick-Theme-Galileo.git",
+    "branch": "latest"
 }
 enable_jsdelivr = {
     "enabled": False,
