34b9d436b896b1611b9a8ce1de2be994153283ec
edmondsket <dannew123@gmail.com>
2021-12-06 15:16:34 +0800

    初始配置

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "帅哥Tap我"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "Eddie"
+email = "dannew123@gmail.com"
+author_homepage = "https://edmondsket.github.io"
+description = "过一种平替人生"
+key_words = ['平替', '免费', '便宜', '贱']
 language = 'zh-CN'
 external_links = [
     {
